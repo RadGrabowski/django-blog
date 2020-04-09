059dea5666b806a72476b1b3a67dc4b7da7e0e47
RadGrabowski <radek18g@gmail.com>
2020-04-09 13:41:27 +0200

    now recommending similar posts by tags

--- blog/post_populator.py
+++ blog/post_populator.py
@@ -1,14 +1,15 @@
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_website.settings')
 
 import django
+
 django.setup()
 
 from faker import Faker
 from blog.models import Post, User, Comment
 from datetime import datetime
 import random
-
 
 fake = Faker()
 
@@ -23,11 +24,22 @@ if __name__ == '__main__':
     #     p.publish = datetime.now()
     #     p.status = 'published'
     #     p.save()
-    for comment in range(120):
-        c = Comment()
-        c.name = fake.name()
-        c.post = random.choice(Post.published.all())
-        c.email = fake.email()
-        c.body = ' '.join(fake.text().split()[:10]) + '.'
-        c.active = True
-        c.save()
+
+    # for comment in range(120):
+    #     c = Comment()
+    #     c.name = fake.name()
+    #     c.post = random.choice(Post.published.all())
+    #     c.email = fake.email()
+    #     c.body = ' '.join(fake.text().split()[:10]) + '.'
+    #     c.active = True
+    #     c.save()
+
+    for post in Post.objects.all():
+        tags = []
+        choices = ['music', 'health', 'sport', 'medicine', 'politics', 'money', 'education', 'science', 'humor',
+                   'technology', 'art', 'food', 'nature', 'travelling', 'psychology', 'architecture', 'life', 'animals']
+        random.shuffle(choices)
+        for tag in range(3):
+            tags.append(choices.pop())
+        # print(post, tags)
+        post.tags.add(*tags)
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,37 @@
 from django.shortcuts import render, redirect, get_object_or_404
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+# from django.views.generic import ListView
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -34,17 +42,25 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
 
     comments = post.comments.filter(active=True)
+    new_comment = True
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     if request.method == 'POST':
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
             new_comment.post = post
             new_comment.save()
-        return redirect(post.get_absolute_url())
+        # return redirect(post.get_absolute_url())
     else:
+        new_comment = None
         comment_form = CommentForm()
-        return render(request, 'blog/post/detail.html',
-                      {'post': post, 'comments': comments, 'comment_form': comment_form})
+
+    return render(request, 'blog/post/detail.html',
+                  {'post': post, 'comments': comments, 'comment_form': comment_form, 'similar_posts': similar_posts,
+                   'new_comment': new_comment})
 
 
 def post_share(request, post_id):
