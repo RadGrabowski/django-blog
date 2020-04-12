d5071b1f0fb7b7b72df2be29699dc38d9b1565d6
RadGrabowski <radek18g@gmail.com>
2020-04-12 23:18:41 +0200

    changed layout

--- blog/forms.py
+++ blog/forms.py
@@ -6,11 +6,13 @@ class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    comments = forms.CharField(required=False, widget=forms.Textarea(attrs={'rows': 4}))
     bot_catcher = forms.CharField(required=False, widget=forms.HiddenInput)
 
 
 class CommentForm(forms.ModelForm):
+    body = forms.CharField(required=False, widget=forms.Textarea(attrs={'rows': 4}))
+
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
--- blog_website/urls.py
+++ blog_website/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+sitemaps = {'posts': PostSitemap}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include(('blog.urls', 'blog'), namespace='blog'))
+                  path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
+                  path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
