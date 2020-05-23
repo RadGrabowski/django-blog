bc4be9e6134e846dd999f03e1e302f2d6c258922
RadGrabowski <radek18g@gmail.com>
2020-05-23 21:06:34 +0200

    changed views to CBV's

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,11 @@ from .feeds import LatestPostsFeed
 
 urlpatterns = [
     # path('', views.post_list, name='post_list'),
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<str:post>',
-         views.post_detail, name='post_detail'),
-    path('<int:post_id>/share/', views.post_share, name='post_share'),
-    path('tag/<str:tag_slug>/', views.post_list, name='post_list_by_tag'),
+         views.PostDetailView.as_view(), name='post_detail'),
+    path('<pk>/share/', views.PostShareView.as_view(), name='post_share'),
+    path('tag/<str:tag_slug>/', views.PostListView.as_view(), name='post_list_by_tag'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
-    path('search/', views.post_search, name='post_search'),
+    path('search/', views.PostSearchView.as_view(), name='post_search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-# from django.views.generic import ListView
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
@@ -8,86 +8,91 @@ from taggit.models import Tag
 from django.db.models import Count
 
 
-# class PostListView(ListView):
-#     queryset = Post.published.all()
-#     context_object_name = 'posts'
-#     paginate_by = 3
-#     template_name = 'blog/post/list.html'
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 4
+    template_name = 'blog/post/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag = None
+
+        if 'tag_slug' in self.kwargs:
+            tag = get_object_or_404(Tag, slug=self.kwargs['tag_slug'])
+            context['posts'] = self.queryset.filter(tags__in=[tag])
+
+        context['tag'] = tag
+        return context
 
 
-def post_list(request, tag_slug=None):
-    object_list = Post.published.all()
-    tag = None
+class PostDetailView(DetailView):
+    template_name = 'blog/post/detail.html'
+    context_object_name = 'post'
+    model = None
 
-    if tag_slug:
-        tag = get_object_or_404(Tag, slug=tag_slug)
-        object_list = object_list.filter(tags__in=[tag])
+    def dispatch(self, request, *args, **kwargs):
+        self.model = get_object_or_404(Post, slug=self.kwargs['post'], status='published',
+                                       publish__year=self.kwargs['year'],
+                                       publish__month=self.kwargs['month'],
+                                       publish__day=self.kwargs['day'])
+        comments = self.model.comments.filter(active=True)
+        post_tags_ids = self.model.tags.values_list('id', flat=True)
+        similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(pk=self.model.pk)
+        similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
 
-    paginator = Paginator(object_list, 4)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
-
-
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post,
-                             status='published',
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-
-    comments = post.comments.filter(active=True)
-    new_comment = True
-    post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
-
-    if request.method == 'POST':
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            new_comment = comment_form.save(commit=False)
-            new_comment.post = post
-            new_comment.save()
-        # return redirect(post.get_absolute_url())
-    else:
-        new_comment = None
-        comment_form = CommentForm()
-
-    return render(request, 'blog/post/detail.html',
-                  {'post': post, 'comments': comments, 'comment_form': comment_form, 'similar_posts': similar_posts,
-                   'new_comment': new_comment})
+        if request.method == 'POST':
+            new_comment = True
+            comment_form = CommentForm(data=request.POST)
+            if comment_form.is_valid():
+                new_comment = comment_form.save(commit=False)
+                new_comment.post = self.model
+                new_comment.save()
+        else:
+            new_comment = None
+            comment_form = CommentForm()
+        return self.render_to_response({'post': self.model, 'comments': comments, 'comment_form': comment_form,
+                                        'similar_posts': similar_posts, 'new_comment': new_comment})
 
 
-def post_share(request, post_id):
-    post = get_object_or_404(Post, id=post_id, status='published')
+class PostShareView(DetailView):
+    template_name = 'blog/post/share.html'
+    model = Post
+    context_object_name = 'post'
     sent = False
     cd = {}
 
-    if request.method == 'POST':
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(status='published')
+
+    def post(self, request, *args, **kwargs):
         form = EmailPostForm(request.POST)
+        post = self.get_object()
         if form.is_valid():
-            cd = form.cleaned_data
+            self.cd = form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url())
-            subject = f'{cd["name"]} ({cd["email"]}) recommends you reading "{post.title}"'
-            message = f'Read "{post.title}" at {post_url}\n\n{cd["name"]}\'s comments: {cd["comments"]}'
-            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
-            sent = True
-    else:
+            subject = f'{self.cd["name"]} ({self.cd["email"]}) recommends you reading "{post.title}"'
+            message = f'Read "{post.title}" at {post_url}\n\n{self.cd["name"]}\'s comments: {self.cd["comments"]}'
+            send_mail(subject, message, 'admin@myblog.com', [self.cd['to']])
+            self.sent = True
+        return self.render_to_response({'post': self.get_object(), 'form': form, 'sent': self.sent, 'cd': self.cd})
+
+    def get(self, request, *args, **kwargs):
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent, 'cd': cd})
+        return self.render_to_response({'post': self.get_object(), 'form': form, 'sent': self.sent, 'cd': self.cd})
 
 
-def post_search(request):
-    if request.method == 'POST' and request.POST['phrase']:
-        phrase = request.POST['phrase']
-        searched_posts = [post for post in Post.published.all() if phrase in post.body.lower() or phrase in post.title]
-        total_results = len(searched_posts)
-        return render(request, 'blog/post/search.html',
-                      {'phrase': phrase, 'searched_posts': searched_posts, 'total_results': total_results})
-    else:
-        return redirect('blog:post_list')
+class PostSearchView(TemplateView):
+    template_name = 'blog/post/search.html'
+
+    def post(self, request, *args, **kwargs):
+        if request.POST['phrase']:
+            phrase = request.POST['phrase']
+            searched_posts = [post for post in Post.published.all() if
+                              phrase in post.body.lower() or phrase in post.title]
+            total_results = len(searched_posts)
+        else:
+            return redirect('blog:post_list')
+        return self.render_to_response(
+            {'phrase': phrase, 'searched_posts': searched_posts, 'total_results': total_results})
